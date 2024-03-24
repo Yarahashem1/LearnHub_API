05f31191009b874e8841f77c60589c97ba3abde2
Yara Hashem Madi <88033413+Yarahashem1@users.noreply.github.com>
2024-03-24 20:43:46 +0200

    Create login2.php

---
+++ auth/login2.php
@@ -0,0 +1,22 @@
+<?php 
+  include "../connect.php";
+
+  $id = htmlspecialchars(strip_tags($_POST['id']));
+  $password = htmlspecialchars(strip_tags($_POST['password']));
+  $stmt = $con->prepare("
+  SELECT * FROM teachers where id = ? and password = ?  
+  "
+   
+  );
+  $stmt->execute(array($id,$password));
+  $data = $stmt->fetch(PDO::FETCH_ASSOC);
+  $users = $stmt->rowCount();
+  if($users > 0) {
+    echo json_encode(array("status" => "success", "data" => $data));
+  } else {
+    echo json_encode(array("status" => "fail"));
+  }
+
+  
+
+?>
